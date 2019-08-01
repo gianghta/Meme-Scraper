9c2b101f28ad3757eabce43c32f91737781e7617
Giang Ta <tagh@ucmail.uc.edu>
2019-08-01 15:09:07 -0400

    Definition function

--- meme-scraper.py
+++ meme-scraper.py
@@ -5,11 +5,10 @@
 
 import requests, re
 from bs4 import BeautifulSoup
+from difflib import SequenceMatcher
 
 HEADERS = {'User-Agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 '
         '(KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36')}
-
-SEARCH_SIMILARITY_THRESHOLD = .4
 
 def search_meme(text):
     try:
@@ -26,24 +25,16 @@ def search_meme(text):
         return search_path.replace('-', ' '), 'https://knowyourmeme.com{}'.format(search_path)
     return None, None
 
-def regexCompare(text):
-    memeName, url = search_meme(text)
-    print(memeName)
-    memeRegex = re.compile(r'text')
-    output = memeRegex.search(memeName)
-    print(output.group())
-
-# def search(text):
-#     """Return a meme definition from a meme keywords.
-#     """
-#     meme_name, url = search_meme(text)
-#     if meme_name and SequenceMatcher(None, text, meme_name).ratio() >= SEARCH_SIMILARITY_THRESHOLD:
-#         r = requests.get(url, headers=HEADERS)
-#         soup = BeautifulSoup(r.text, 'html.parser')
-#         entry = soup.find('h2', {'id': 'about'})
-#         return '%s. %s' % (meme_name.split('/')[-1].title(), entry.next.next.next.text)
+def search(text):
+    """Return a meme definition from a meme keywords.
+    """
+    meme_name, url = search_meme(text)
+    if meme_name and SequenceMatcher(None, text, meme_name).ratio() >= 0.4:
+        r = requests.get(url, headers=HEADERS)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        entry = soup.find('h2', {'id': 'about'})
+        return '%s. %s' % (meme_name.split('/')[-1].title(), entry.next.next.next.text)
 
 if __name__ == "__main__":
-    search = input('Enter search for meme: ')
-    search_meme(search)
-    regexCompare(search)
+    searchKeyword = input('Enter search for meme: ')
+    print(search(searchKeyword))
