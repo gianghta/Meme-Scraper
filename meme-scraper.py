642148077243609c73b77694ba9fc87837dc72bf
Giang Ta <tagh@ucmail.uc.edu>
2019-08-01 15:15:33 -0400

    Add class

--- meme-scraper.py
+++ meme-scraper.py
@@ -10,6 +10,13 @@ from difflib import SequenceMatcher
 HEADERS = {'User-Agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 '
         '(KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36')}
 
+class Meme:
+    def __init__(self):
+        self.definition = None
+        self.imageURL = None
+        self.siteURL = None
+        self.soup = None
+
 def search_meme(text):
     try:
         re = requests.get(f'http://knowyourmeme.com/search?q={text}', headers=HEADERS)
