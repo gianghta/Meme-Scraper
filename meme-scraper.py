3249b477571350de5002e63601746c680284d6b4
Son <son@fr0st.local>
2019-08-03 11:31:57 +0800

    Try to refactor the code

--- meme-scraper.py
+++ meme-scraper.py
@@ -1,59 +1,79 @@
 #!/usr/bin/python
-''' Python scraper that scrape the meme from
-    Know Your Meme site
-'''
+"""
+Python scraper that scrape the meme from
+Know Your Meme site
+"""
 
-import requests, re
+from collections import namedtuple
+import requests
 from bs4 import BeautifulSoup
 
-HEADERS = {'User-Agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 '
-        '(KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36')}
+Meme = namedtuple("Meme", ("definition", "image_url", "site_url"))
 
-class Meme:
-    def __init__(self):
-        self.definition = None
-        self.imageURL = None
-        self.siteURL = None
-        self.soup = None
-    
-    def parse(self, meme_url):
-        # Parse in the result URL and soup object for it
-        self.siteURL = meme_url
-        r = requests.get(self.siteURL, headers=HEADERS)
-        self.soup = BeautifulSoup(r.text, 'html.parser')
+
+class MemeSearcher:
+    headers = {
+        "User-Agent": (
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 "
+            "(KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+        )
+    }
+
+    ## PRIVATE ##
+
+    def __get_soup_from_request(self, url):
+        """Return a BeautifulSoup object from a GET response."""
+        res = requests.get(url, headers=self.headers)
+        res.raise_for_status()
+        soup = BeautifulSoup(res.text, "html.parser")
+        return soup
+
+    def __get_soup_from_request_by_keyword(self, search_keyword):
+        url = f"http://knowyourmeme.com/search?q={search_keyword}"
+        return self.__get_soup_from_request(url)
+
+    def __get_meme_url_from_search_path(self, search_path):
+        return "https://knowyourmeme.com{}".format(search_path)
+
+    ## PUBLIC ##
+
+    def search_by_keyword(self, search_keyword):
+        """Return a <Meme> object found by searching with the given keyword."""
+        soup = self.__get_soup_from_request_by_keyword(search_keyword)
+        search_result = soup.find(class_="entry_list")
+
+        if search_result:
+            search_path = search_result.find("a", href=True)["href"]
+            return self.search_by_url(self.__get_meme_url_from_search_path(search_path))
+
+        raise Exception(f"Sorry but there were no results for {search_keyword}")
+
+    def search_by_url(self, meme_url):
+        """Return a <Meme> object found by searching with the given URL."""
+        soup = self.__get_soup_from_request(meme_url)
 
         # Parse in the definition of the result meme
-        def_tag = self.soup.find('h2', {'id': 'about'})
-        self.definition = def_tag.next_element.next_element.next_element.text
+        def_tag = soup.find("h2", {"id": "about"})
+        definition = def_tag.next_element.next_element.next_element.text
 
         # Parse in the according image URL
-        meme_header_tag = self.soup.find('header', class_='rel c')
-        img_tag = meme_header_tag.find('a', class_='photo left')['href']
-        self.imageURL = img_tag
-
-class MemeSearcher(Meme):
-    def __init__(self, search_keyword):
-        super().__init__()
-        self.search_keyword = search_keyword
-        self.meme_result_url = None
-
+        img_tag = None
         try:
-            re = requests.get(f'http://knowyourmeme.com/search?q={search_keyword}', headers=HEADERS)
-            re.raise_for_status
-        except Exception as e:
-            print(f'Something wrong: {e}')
-        soup = BeautifulSoup(re.text, 'html.parser')
-        search_result = soup.find(class_='entry_list')
-        if search_result:
-            search_path = search_result.find('a', href=True)['href']
-        else:
-            raise Exception(f'Sorry but there were no results for {search_keyword}')
+            meme_header_tag = soup.find("header", class_="rel c")
+            img_tag = meme_header_tag.find("a", class_="photo left")["href"]
+        except Exception:
+            pass
 
-        self.meme_result_url = 'https://knowyourmeme.com{}'.format(search_path)
-        self.parse(self.meme_result_url)
+        return Meme(definition=definition, site_url=meme_url, image_url=img_tag)
+
 
 if __name__ == "__main__":
-    
+
     # Sample test for the scraper
-    search_word = input('Enter search for meme: ')
-    print(MemeSearcher(search_word).definition)
+    # search_word = "sanic"
+    search_word = "duwang"
+
+    searcher = MemeSearcher()
+    meme_found = searcher.search_by_keyword(search_word)
+
+    print(meme_found)
